3cde6fd7fb4a5851c6bf0978d7551f222f879fe2
tech@k-visa.co.kr <tech@k-vias.co.kr>
2025-01-22 18:06:04 +0900

    배포

--- config/settings.py
+++ config/settings.py
@@ -230,15 +230,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'kvisa',
-        'USER': 'root',
-        'PASSWORD': 'pass!@12worD',
-        'HOST': 'localhost',
+        'NAME': 'kvisa_app',
+        'USER': 'admin',
+        'PASSWORD': 'kim12011215',
+        'HOST': 'database-1.cliq0kecaxpp.ap-northeast-2.rds.amazonaws.com',
         'PORT': '3306',
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
+        # 'OPTIONS': {
+        #     'charset': 'utf8mb4',
+        #     'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        # }
     }
 }
 
