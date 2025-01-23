d6bb1494a99359215ec54108d333c0d501073d84
tech@k-visa.co.kr <tech@k-vias.co.kr>
2025-01-23 10:40:46 +0900

    db 사용방식 변경

--- kvisa/urls/auth_urls.py
+++ kvisa/urls/auth_urls.py
@@ -4,13 +4,16 @@ from ..views.auth_views import (
     AppleLogin,
     KakaoLogin,
     GoogleLogin,
-    FacebookLogin
+    FacebookLogin,
+    TokenValidationView
 )
 
 auth_urlpatterns = [
     # dj-rest-auth 기본 URLs (로그인, 로그아웃 등)
     path('', include('dj_rest_auth.urls')),
-
+    path('registration/', include('dj_rest_auth.registration.urls')),
+    path('token/validate/', TokenValidationView.as_view(), name='TokenValidationView'),
+    
     # 소셜 로그인 URLs
     path('kakao/', KakaoLogin.as_view(), name='kakao_login'),
     path('google/', GoogleLogin.as_view(), name='google_login'),
